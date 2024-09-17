809eca3acffc46f504a675d9cf79d4ee77ce99d7
Mischa Haenen <mischahaenen@gmail.com>
2024-09-17 11:22:23 +1000

    modify main script

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import lgpio as GPIO
+import time
+
+TRIG = 15
+ECHO = 14
+
+h = GPIO.gpiochip_open(0)
+GPIO.gpio_claim_output(h, TRIG)
+GPIO.gpio_claim_output(h, ECHO)
+
+def get_distance():
+    GPIO.gpio_write(h, TRIG, 0)
+    time.sleep(2)
+
+    GPIO.gpio_write(h, TRIG, 1)
+    time.sleep(0.0001)
+    GPIO.gpio.write(h, TRIG, 0)
+    :
+
 if __name__ == '__main__':
     print('works')
